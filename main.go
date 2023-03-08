c27ab77806ac6750413d1b4aab90833e5189a0bb
dinghenc <308024784@qq.com>
2023-03-08 23:15:25 +0800

    support lock screen after intervals

--- main.go
+++ main.go
@@ -2,42 +2,68 @@ package main
 
 import (
 	"flag"
+	"log"
 	"math/rand"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/go-vgo/robotgo"
 )
 
-const MoveStep = 1
+const (
+	MoveStep   = 1
+	TimeFormat = "2006-01-02 15:04:05"
+)
 
-var MoveDirection [4]robotgo.Point = [4]robotgo.Point{
-	{X: -MoveStep, Y: 0},
-	{X: 0, Y: -MoveStep},
-	{X: MoveStep, Y: 0},
-	{X: 0, Y: MoveStep},
-}
+var (
+	MoveDirection [4]robotgo.Point = [4]robotgo.Point{
+		{X: -MoveStep, Y: 0},
+		{X: 0, Y: -MoveStep},
+		{X: MoveStep, Y: 0},
+		{X: 0, Y: MoveStep},
+	}
+	ProgramName = filepath.Base(os.Args[0])
+)
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
-	var freshTime int
-	flag.IntVar(&freshTime, "fresh", 1, "mouse fresh time, must be greater zero")
+	var freshDuration time.Duration
+	var exitDuration time.Duration
+	flag.DurationVar(&freshDuration, "fresh", time.Minute, "mouse fresh time")
+	flag.DurationVar(&exitDuration, "duration", 2*time.Hour, "time duration of random-mouse exit")
 	flag.Parse()
-	if freshTime <= 0 {
-		freshTime = 1
-	}
 
-	ticker := time.NewTicker(time.Duration(freshTime) * time.Second)
+	now := time.Now()
+	exitTime := now.Add(exitDuration)
+	log.Printf("now is %s\n", now.Format(TimeFormat))
+	log.Printf("%s will exit when time is %s, it's fresh time is %ds\n",
+		ProgramName, exitTime.Format(TimeFormat), freshDuration/time.Second)
+
+	exitTimer := time.NewTimer(exitDuration)
+	ticker := time.NewTicker(freshDuration)
 	for {
 		select {
 		case <-ticker.C:
 			currentPosition := GetMousePos()
 			nextPosition := GetNextPosition(currentPosition)
 			MoveMouse(nextPosition)
+		case <-exitTimer.C:
+			exitTimer.Stop()
+			ticker.Stop()
+			log.Printf("now is %s, time is up, %s exit...\n", time.Now().Format(TimeFormat), ProgramName)
+			time.Sleep(5 * time.Second)
+			LockScreen()
+			os.Exit(0)
 		}
 	}
+}
+
+func LockScreen() {
+	robotgo.KeyTap("q", "lcmd", "lctrl")
 }
 
 func GetMousePos() robotgo.Point {
